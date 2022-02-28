c95fd2226bbec34b0519a7a94e1dcc72f4d671d4
Julien Bordet <zejames@gmail.com>
2022-02-28 10:18:04 +0100

    setup.py code cleaning

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup
 from app._version import __version__
 
-APP = ['app/menuping.py']
-DATA_FILES = [ ('resources') ]
-OPTIONS = {
-    'argv_emulation': True,
-    'iconfile': 'icon.icns',
-    'plist': {
-        'CFBundleShortVersionString': __version__,
-        'CFBundleIdentifier': 'info.bordet.menuping',
-        'LSUIElement': True,
-    },
-    'packages': ['rumps', 'ping3', 'appdirs'],
-}
+if __name__ == "__main__":
 
-setup(
-    app=APP,
-    name='MenuPing',
-    data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'], install_requires=['rumps', 'ping3', 'appdirs']
-)
+    APP = ['app/menuping.py']
+    DATA_FILES = [ ('resources') ]
+    OPTIONS = {
+        'argv_emulation': True,
+        'iconfile': 'icon.icns',
+        'plist': {
+            'CFBundleShortVersionString': __version__,
+            'CFBundleIdentifier': 'info.bordet.menuping',
+            'LSUIElement': True,
+        },
+        'packages': ['rumps', 'ping3', 'appdirs'],
+    }
+
+    setup(
+        app=APP,
+        name='MenuPing',
+        data_files=DATA_FILES,
+        options={'py2app': OPTIONS},
+        setup_requires=['py2app'], install_requires=['rumps', 'ping3', 'appdirs']
+    )
