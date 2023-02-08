6759367ad078926b19f0e76a978bfd440c15f6ac
Julien Bordet <zejames@gmail.com>
2023-02-08 09:57:26 +0100

    [build] split build and sign into 2 scripts

---
+++ utils/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "---------------------"
+echo "| Building the app  |"
+echo "---------------------"
+
+echo "Checking pre-requisites"
+
+if [[ "$VIRTUAL_ENV" == "" ]]; then
+  echo " -> you should be in virtual env to build ${NAME}"
+  exit 1
+fi
+
+echo "Building through py2app"
+python3 setup.py py2app --arch universal2 >/dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo " -> error building ${NAME}"
+    exit 1
+fi
